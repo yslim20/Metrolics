813606634b8b5e712ab8a70967471e818540fb0c
younsoolim <ylim27@my.bcit.ca>
2021-02-03 06:28:21 -0800

    Navigation updated

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.querySelector(".mobile-nav").addEventListener("click",
+function (e) {
+  const element = document.querySelector(".main-nav");
+  element.classList.toggle("s-show");
+  e.preventDefault();
+}, false);
+
+
